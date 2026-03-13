aa7a0ff38cae9ceaf7f609ed4025946731fc5905
rahulKumar1404 <rahul.kumar.uemk.cs.2023@gmail.com>
2026-03-13 11:57:21 +0530

    initial commit

---
+++ backend/src/main/java/com/spring_ai/backend/SpringAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring_ai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiBackendApplication.class, args);
+	}
+
+}
